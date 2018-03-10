0c4ad2831c843841ecd6f5a6fb00810e6649e9c0
VincentAudette <36488653+VincentAudette@users.noreply.github.com>
2018-03-09 19:03:20 -0500

    Rename GccApplication1/driver.c to GccApplication1/Source/driver.c

--- GccApplication1/Source/driver.c
+++ GccApplication1/Source/driver.c
@@ -1,6 +1,6 @@
 /**
 	\file  driver.c
-	\brief Code source de fonctions qui pilotent directement du matériel
+	\brief Code source de fonctions qui pilotent directement du matÃ©riel
 	
 	\author *********************************************************
 	\author ***             VINCENT AUDETTE						  ***
@@ -23,29 +23,29 @@ Includes
 
 
 /******************************************************************************
-Définitions de fonctions
+DÃ©finitions de fonctions
 ******************************************************************************/
 
 void adc_init(void){
 	
-	// Configuration des broches utilisées du port A en entrée (Entre PA0 et PA7)	
+	// Configuration des broches utilisÃ©es du port A en entrÃ©e (Entre PA0 et PA7)	
 	DDRA = clear_bit(DDRA, PA1);//Servo
 	
 	DDRA = clear_bit(DDRA, PA0);//Moteur
 
-	// Sélectionner la référence de tension: la tension d'alimentation (AVCC)
+	// SÃ©lectionner la rÃ©fÃ©rence de tension: la tension d'alimentation (AVCC)
 	ADMUX = clear_bit(ADMUX, REFS1);
 	ADMUX = set_bit(ADMUX, REFS0);
 	
-	// Choisir le format du résultat de conversion: shift à gauche pour que
+	// Choisir le format du rÃ©sultat de conversion: shift Ã  gauche pour que
 	// les 8 MSB se retrouvent dans le registre ADCH
 	ADMUX = set_bit(ADMUX, ADLAR);
 
 	// Choisir un facteur de division d'horloge (64) afin que l'horloge ait
-	// une fréquence entre 50kHz et 200kHz. Donc 8MHz/64 = 125kHz.
+	// une frÃ©quence entre 50kHz et 200kHz. Donc 8MHz/64 = 125kHz.
 	ADCSRA = set_bit(ADCSRA, ADPS1);
 	ADCSRA = set_bit(ADCSRA, ADPS2);
-	ADCSRA = set_bit(ADCSRA, ADPS0); //RAJOUTÉ POUR RENDRE LE SERVO PLUS COOL
+	ADCSRA = set_bit(ADCSRA, ADPS0); //RAJOUTÃ‰ POUR RENDRE LE SERVO PLUS COOL
 
 	// Activer le CAN
 	ADCSRA = set_bit(ADCSRA, ADEN);
@@ -53,7 +53,7 @@ void adc_init(void){
 
 uint8_t adc_read(uint8_t pin_name){
 	
-	// Choisir l'entrée analogique (broche) à convertir
+	// Choisir l'entrÃ©e analogique (broche) Ã  convertir
 	ADMUX = set_bit(ADMUX, MUX0);//Servo
 	ADMUX = clear_bit(ADMUX, MUX1);
 	ADMUX = clear_bit(ADMUX, MUX2);
@@ -62,7 +62,7 @@ uint8_t adc_read(uint8_t pin_name){
 	
 	ADMUX = write_bits(ADMUX, 0b0001111, pin_name);//Moteur
 
-	// Démarrage d'une conversion 
+	// DÃ©marrage d'une conversion 
 	ADCSRA = set_bit(ADCSRA, ADSC);
 	
 	// Attente de la fin de la conversion
@@ -70,7 +70,7 @@ uint8_t adc_read(uint8_t pin_name){
 		
 	}
 
-	// Lecture et renvoie du résultat
+	// Lecture et renvoie du rÃ©sultat
 	return ADCH;
 }
 
@@ -87,13 +87,13 @@ void servo_init(void){
 	TCCR1B = set_bit(TCCR1B, WGM13);
 	TCCR1B = set_bit(TCCR1B, WGM12);
 
-	// Configuration de la valeur maximale du compteur (top) à 20000
+	// Configuration de la valeur maximale du compteur (top) Ã  20000
 	ICR1 = 20000;
 	
-	// Initialiser la valeur du compteur à 0
+	// Initialiser la valeur du compteur Ã  0
 	TCNT1 = 0;
 
-	// Démarrer le compteur et fixer un facteur de division de fréquence à 8
+	// DÃ©marrer le compteur et fixer un facteur de division de frÃ©quence Ã  8
 	TCCR1B = set_bit(TCCR1B, CS11);
 	
 }
@@ -123,18 +123,18 @@ void pwm_init(bool init_a, bool init_b){
 		TCCR0= set_bit(TCCR0, WGM00);
 		TCCR0= set_bit(TCCR0, WGM01);
 		
-		//Deuxième moteur
+		//DeuxiÃ¨me moteur
 		TCCR2= set_bit(TCCR2, WGM20);
 		TCCR2= set_bit(TCCR2, WGM21);
 
-		// Démarrer le compteur et fixer un facteur de division de fréquence à 1024
+		// DÃ©marrer le compteur et fixer un facteur de division de frÃ©quence Ã  1024
 		
 		//Premier moteur
 		TCCR0 = clear_bit(TCCR0, CS02);
 		TCCR0= set_bit(TCCR0, CS01);
 		TCCR0= set_bit(TCCR0, CS00);
 		
-		//Deuxième moteur
+		//DeuxiÃ¨me moteur
 		TCCR2 = clear_bit(TCCR2, CS22);
 		TCCR2= set_bit(TCCR2, CS21);
 		TCCR2= set_bit(TCCR2, CS20);
@@ -143,13 +143,13 @@ void pwm_init(bool init_a, bool init_b){
 
 void pwm_set_a(uint8_t duty){
 	
-	// Pour avoir un duty de 0, il faut éteindre le PWM et directement piloter la sortie à 0
+	// Pour avoir un duty de 0, il faut Ã©teindre le PWM et directement piloter la sortie Ã  0
 	if(duty == 0){
 		
 		//Mettre 0 dans la broche PD7 (OC2) du port
 		PORTD = clear_bit(PORTD, PD7);
 		
-		//Désactive le comparateur
+		//DÃ©sactive le comparateur
 		TCCR0 = clear_bit(TCCR0, COM01);
 	}
 	
@@ -169,13 +169,13 @@ void pwm_set_a(uint8_t duty){
 
 void pwm_set_b(uint8_t duty){
 
-	// Pour avoir un duty de 0, il faut éteindre le PWM et directement piloter la sortie à 0
+	// Pour avoir un duty de 0, il faut Ã©teindre le PWM et directement piloter la sortie Ã  0
 	if(duty == 0){
 		
 		//Mettre 0 dans la broche PB3 (OC0) du port
 		PORTB = clear_bit(PORTB, PB3);
 		
-		//Désactive le comparateur
+		//DÃ©sactive le comparateur
 		TCCR2 = clear_bit(TCCR2, COM21);
 	}
 	
